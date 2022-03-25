5018c051b2d97564ffb1100fc2dd2d5cd2a9ca8b
Bhavya Gor <bhavya.gor9999@gmail.com>
2022-03-25 21:23:36 +0530

    navbar changes

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,58 +1,68 @@
-import Link from "next/link";
-import { useState } from "react";
-const Navbar = () => {
-  const [isOpen, setIsOpen] = useState(false);
+import React from "react";
+import Image from "next/image";
+import {
+  SearchIcon,
+  PlusCircleIcon,
+  UserGroupIcon,
+  HeartIcon,
+  PaperAirplaneIcon,
+  MenuIcon,
+} from "@heroicons/react/outline";
+import { HomeIcon } from "@heroicons/react/solid";
+function Navbar() {
   return (
-    <div>
-      <nav className="shadow-sm fixed w-full z-10 dark:bg-[#032A5D] bg-[#0082EF]">
-        <div className="w-full">
-          <div className="flex items-center h-20 w-full">
-            <div className="flex items-center  mx-20  justify-between w-full">
-              <div className="flex justify-center items-center flex-shrink-0 ">
-                <Link href="/home">
-                  <h1 className=" font-bold text-3xl cursor-pointer text-white">
-                    Dev
-                    <span className="dark:text-[#0082EF] text-[#032A5D]">
-                      Mates
-                    </span>
-                  </h1>
-                </Link>
-              </div>
-              <div className="hidden md:block">
-                <div className="ml-10 flex items-baseline space-x-4">
-                  <Link href="/profile">
-                    <p className="text-white text-xl cursor-pointer dark:hover:text-[#0082EF] hover:text-[#032A5D]">
-                      Profile
-                    </p>
-                  </Link>
-                  <Link href="/projects">
-                    <p className="text-white text-xl cursor-pointer dark:hover:text-[#0082EF] hover:text-[#032A5D]">
-                      Projects
-                    </p>
-                  </Link>
-                  <Link href="/chat">
-                    <p className="text-white text-xl cursor-pointer dark:hover:text-[#0082EF] hover:text-[#032A5D]">
-                      Chat
-                    </p>
-                  </Link>
-                  <Link href="/newsPage">
-                    <p className="text-white text-xl cursor-pointer dark:hover:text-[#0082EF] hover:text-[#032A5D]">
-                      News
-                    </p>
-                  </Link>
-                  <Link href="/login">
-                    <p className="text-white text-xl cursor-pointer dark:hover:text-[#0082EF] hover:text-[#032A5D]">
-                      Login
-                    </p>
-                  </Link>
-                </div>
-              </div>
+    <div className="shadow-sm border-b bg-white sticky top-0 z-50">
+      <div className="flex justify-between max-w-6xl mx-5  lg:mx-auto">
+        {/* LEFT*/}
+        <div className="relative hidden lg:inline-grid w-24">
+          <Image
+            src="https://play-lh.googleusercontent.com/9ASiwrVdio0I2i2Sd1UzRczyL81piJoKfKKBoC8PUm2q6565NMQwUJCuNGwH-enhm00"
+            layout="fill"
+            objectFit="contain"
+          />
+        </div>
+        <div className="relative w-10 lg:hidden flex-shrink-0">
+          <Image
+            src="https://cdn-icons-png.flaticon.com/512/87/87390.png"
+            layout="fill"
+            objectFit="contain"
+          />
+        </div>
+
+        {/* MIDDLE*/}
+        <div className="max-w-xs">
+          <div className="relative mt-1 p-3 rounded-md">
+            <div className="absolute inset-y-0 pl-3 flex items-center pointer-events-none">
+              <SearchIcon className="h-5 w-5 text-gray-500" />
             </div>
+            <input
+              className="bg-gray-50 block w-full pl-10 sm:text-sm border-gray-300 focus:ring-black focus:border-black rounded-md"
+              placeholdertype="text"
+              placeholder="Search"
+            />
           </div>
         </div>
-      </nav>
+
+        {/* RIGHT*/}
+        <div className="flex items-center justify-end space-x-4">
+          <HomeIcon className="navBtn" />
+          <MenuIcon className="h-6 md:hidden cursor-pointer" />
+          <div className="relative navBtn">
+            <PaperAirplaneIcon className="navBtn rotate-45" />
+          </div>
+
+          <PlusCircleIcon className="navBtn" />
+          <UserGroupIcon className="navBtn" />
+          <HeartIcon className="navBtn" />
+          <img
+            src="https://i.guim.co.uk/img/media/a7fe7170defa865d2b96b829f05c5d8fa82d8edf/0_20_2201_1321/master/2201.jpg?width=465&quality=45&auto=format&fit=max&dpr=2&s=72bbae118ff1631bcc0d1f837159a727"
+            alt=""
+            className="h-10 rounded-full cursor-pointer"
+          />
+        </div>
+      </div>
     </div>
   );
-};
+}
 
 export default Navbar;
--- pages/home.js
+++ pages/home.js
@@ -2,7 +2,7 @@ import Navbar from "../components/Navbar";
 import Title from "../components/Title";
 const home = () => {
   return (
-    <div className="min-h-screen bg-[#0082EF] dark:bg-[#032A5D]">
+    <div className="">
       <Title title="Home" />
       <Navbar />
     </div>
